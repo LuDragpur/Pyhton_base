f9b6876897196308e65b4263e78f2c6f4bd11f45
LuDragpur <75264824+LuDragpur@users.noreply.github.com>
2020-12-13 13:45:59 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,6 @@
+with open('text_1.txt', 'w', encoding='utf-8') as f:
+    while True:
+        line = input('Input new string or empty string to done: ')
+        if not line:
+            break
+        print(line, file=f)
